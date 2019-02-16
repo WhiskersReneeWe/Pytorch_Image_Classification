16bd4d85278d120c31e85083c0629985f805c0d8
Renee S. Liu <reneesyliu@gmail.com>
2019-02-16 10:47:38 -0800

    Update train.py

--- train.py
+++ train.py
@@ -40,22 +40,36 @@ import torch.nn.functional as F
 inp_size = 224 * 224
 
 # define the CNN architecture
+import torch.nn as nn
+import torch.nn.functional as F
+
+# define the CNN architecture
 class Net(nn.Module):
     ### TODO: choose an architecture, and complete the class
     def __init__(self):
         super(Net, self).__init__()
         ## Define layers of a CNN
-        self.fc1 = nn.Linear(inp_size, 1024)
-        self.fc2 = nn.Linear(1024, 133)
-        self.output = nn.LogSoftmax(dim = 1)
+        hidden_1 = 512
+        self.conv1 = nn.Conv2d(3, 16, 3, padding = 1)
+        self.conv2 = nn.Conv2d(16, 32, 3, padding = 1)
+        self.conv3 = nn.Conv2d(32, 64, 3, padding = 1)
+        self.pool = nn.MaxPool2d(2,2)
+        self.fc1 = nn.Linear(64 * 4 * 4, hidden_1)
+        self.fc2 = nn.Linear(hidden_1, 133)
+        self.dropout = nn.Dropout(0.2)
     
     def forward(self, x):
         ## Define forward behavior
-        x = x.view(-1, inp_size)
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        x = self.dropout(x)
+        x = x.view(-1, 64 * 4 * 4)
+        x = self.dropout(x)
         x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.output(x)
+        x = self.fc2(x)
         return x
+
 
 #-#-# You so NOT have to modify the code below this line. #-#-#
 
